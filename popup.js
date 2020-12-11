37bc809a8a7e0401ad3733ae8c8dd19dd6b219e6
Iuri Menin <iuri.menin@softfocus.com.br>
2020-12-11 17:24:30 -0300

    commit inicial

--- popup.js
+++ popup.js
@@ -4,40 +4,38 @@ new Vue({
     el: '#app',
     data: {
         inicio: true,
-        pessoas: [
-            { nome: 'Andre' },
-            { nome: 'Andrieli' },
-            { nome: 'Alessandro' },
-            { nome: 'Cezar' },
-            { nome: 'Elias' },
-            { nome: 'Emanoeli' },
-            { nome: 'Emanuel' },
-            { nome: 'Jaylon' },
-            { nome: 'Joisson' },
-            { nome: 'Laura' },
-            { nome: 'Luana' },
-            { nome: 'Lucas' },
-            { nome: 'Mateus' },
-        ],
-        facilitador: null,
-        secretario: null
+        horaSaida: null,
     },
     methods: {
-        sortear(tipo = null) {
+        calcularSaida() {
             this.inicio = false;
-            if (!tipo || tipo === 'facilitador') {
-                this.facilitador = this.novoParticipante();
-            }
-            
-            if (!tipo || tipo === 'secretario') {
-                this.secretario = this.novoParticipante();
-            }
-        },
-        novoParticipante() {
-            const index = Math.floor(Math.random() * this.pessoas.length);
-            const participante = this.pessoas[index];
-            this.pessoas.splice(index, 1);
-            return participante;
+            var horaChegada = document.getElementById("horaChegada").value;
+            var horaSaida = document.getElementById("horaSaida").value;
+            var horaRetorno = document.getElementById("horaRetorno").value;
+    
+            const dateChegada = new Date();
+            const dateSaida = new Date();
+            const dateRetorno = new Date();
+    
+            dateChegada.setHours(horaChegada.split(":")[0]);
+            dateChegada.setMinutes(horaChegada.split(":")[1]);
+            dateChegada.setSeconds(0);
+    
+            dateSaida.setHours(horaSaida.split(":")[0]);
+            dateSaida.setMinutes(horaSaida.split(":")[1]);
+            dateSaida.setSeconds(0);
+    
+            dateRetorno.setHours(horaRetorno.split(":")[0]);
+            dateRetorno.setMinutes(horaRetorno.split(":")[1]);
+            dateRetorno.setSeconds(0);
+    
+            var horaManha = dateSaida.getTime() - dateChegada.getTime();
+    
+            var millisecondToFinish = (28800000 - horaManha);
+    
+            dateRetorno.setTime(dateRetorno.getTime() + millisecondToFinish);
+    
+            this.horaSaida = dateRetorno.toLocaleTimeString();
         }
     }
 });
